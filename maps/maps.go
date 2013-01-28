ac84c6fbd67c308e7b7aa3f343070ea818122fe1
Rodolfo Carvalho <rhcarvalho@gmail.com>
2013-01-28 18:13:13 +0800

    Implement Maps exercise.

--- maps/maps.go
+++ maps/maps.go
@@ -2,10 +2,15 @@ package main
 
 import (
     "code.google.com/p/go-tour/wc"
+    "strings"
 )
 
 func WordCount(s string) map[string]int {
-    return map[string]int{"x": 1}
+    m := make(map[string]int)
+    for _, w := range strings.Fields(s) {
+        m[w] += 1
+    }
+    return m
 }
 
 func main() {
