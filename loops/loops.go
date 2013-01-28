491cf4e1ae72a00bf86452ac9ea0c8084da857d8
Rodolfo Carvalho <rhcarvalho@gmail.com>
2013-01-28 18:17:46 +0800

    Implement Loops exercise.

--- loops/loops.go
+++ loops/loops.go
@@ -2,9 +2,15 @@ package main
 
 import (
     "fmt"
+    "math"
 )
 
 func Sqrt(x float64) float64 {
+    z := 1.0
+    for Δ := 0.1; math.Abs(Δ) > 1e-10; Δ = (z*z - x) / (2 * z) {
+        z -= Δ
+    }
+    return z
 }
 
 func main() {
