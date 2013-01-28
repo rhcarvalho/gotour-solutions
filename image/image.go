219f4f53153c560d8363ad5e5804f0cbedd6491a
Rodolfo Carvalho <rhcarvalho@gmail.com>
2013-01-28 17:47:28 +0800

    Implement Image exercise.

--- image/image.go
+++ image/image.go
@@ -3,11 +3,27 @@ package main
 import (
     "code.google.com/p/go-tour/pic"
     "image"
+    "image/color"
 )
 
-type Image struct{}
+type Image struct {
+    w, h int
+}
+
+func (i Image) ColorModel() color.Model {
+    return color.RGBAModel
+}
+
+func (i Image) Bounds() image.Rectangle {
+    return image.Rect(0, 0, i.w, i.h)
+}
+
+func (i Image) At(x, y int) color.Color {
+    v := uint8(x + y)
+    return color.RGBA{v, v, uint8(x), 255}
+}
 
 func main() {
-    m := Image{}
+    m := Image{w: 100, h: 100}
     pic.ShowImage(m)
 }
