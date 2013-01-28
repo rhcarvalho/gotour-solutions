29d99e368ba5420303fa3a294ecccc3026bdc96b
Rodolfo Carvalho <rhcarvalho@gmail.com>
2013-01-28 17:51:24 +0800

    Implement Errors exercise.

--- errors/errors.go
+++ errors/errors.go
@@ -2,10 +2,24 @@ package main
 
 import (
     "fmt"
+    "math"
 )
 
+type ErrNegativeSqrt float64
+
+func (e ErrNegativeSqrt) Error() string {
+    return fmt.Sprintf("cannot Sqrt negative number: %v", float64(e))
+}
+
 func Sqrt(f float64) (float64, error) {
-    return 0, nil
+    if f < 0 {
+        return 0, ErrNegativeSqrt(f)
+    }
+    z := 1.0
+    for t := 1.1; math.Abs(t) > 1e-5; t = (z*z - f) / (2 * z) {
+        z -= t
+    }
+    return z, nil
 }
 
 func main() {
