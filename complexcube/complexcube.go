38ef4fc0595892b73083279ce6a60fed91a3f922
Rodolfo Carvalho <rhcarvalho@gmail.com>
2013-01-28 18:04:47 +0800

    Implement Complex Cube Root exercise.

--- complexcube/complexcube.go
+++ complexcube/complexcube.go
@@ -1,10 +1,21 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "math/cmplx"
+)
 
 func Cbrt(x complex128) complex128 {
+    z := 1 + 0i
+    for _ = range make([]int, 10) {
+        z -= (z*z*z - x) / (3 * z * z)
+    }
+    return z
 }
 
 func main() {
-    fmt.Println(Cbrt(2))
+    for _, n := range []complex128{1i, 2, 3 + 4i} {
+        cbrtN := Cbrt(n)
+        fmt.Println(cbrtN, cmplx.Pow(cbrtN, 3))
+    }
 }
